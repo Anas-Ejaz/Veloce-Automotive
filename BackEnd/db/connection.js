9d9b2e6b530035205f13aba3670667a2a14445e6
Anas-Ejaz <manasejaz356@gmail.com>
2026-01-22 01:29:41 +0500

    del all

--- BackEnd/db/connection.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Connection = ()=>{
-    try{
-        mongoose.connect("mongodb://localhost:27017/");
-        console.log("Mongodb start");
-    }catch(err){
-        console.log(`error is ${err}`);
-    }
-}
